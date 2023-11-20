078261fa7d1b7b6b171f18e374b7dfd3208f457a
Dxvah <idayraperezdelgado@gmail.com>
2023-11-20 16:39:29 +0000

    _

--- EjercicioRaycast/Assets/Scripts/Salto.cs
+++ EjercicioRaycast/Assets/Scripts/Salto.cs
@@ -8,31 +8,45 @@ public class Salto : MonoBehaviour
     bool isGrounded;
     void Start()
     {
-        rb.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        Vector3 origen = transform.position;
+        Vector3 direccion = Vector3.down;
+        // Vecto3.down
+        RaycastHit col;
+        if (Physics.Raycast(origen, direccion, out col,0.5f))
+        {
+
+          
+            isGrounded = true;
+
+        }
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            //Invoke("Jump", 3f);
             Jump();
-
         }
     }
     void Jump()
     {
+
+        rb.AddForce(Vector3.up * 100);
+        isGrounded = false;
+    }
+
+    private void OnDrawGizmos()
+    {
         Vector3 origen = transform.position;
-        Vector3 direccion = transform.forward;
-        RaycastHit golpe;
-        if (Physics.Raycast(origen, direccion, out golpe))
-        {
+        Vector3 direccion = -transform.up;
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(origen, direccion);
 
-
-
-        }
-
-
-
+        Vector3 direccionMundo = Vector3.down;
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(origen, direccionMundo);
     }
 }
